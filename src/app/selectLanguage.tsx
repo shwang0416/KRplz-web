0e2d3ec210420078a9dbe9a641a1334cbad6ebe6
Hwang Sujung <crystalhwang0416@gmail.com>
2023-08-26 18:00:08 +0900

    feat: select된 언어에 따라 root 페이지 번역

--- src/app/selectLanguage.tsx
+++ src/app/selectLanguage.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
-const SelectLanguage = () => {
-  const [lang, setLang] = useState<string>();
+const SelectLanguage = ({ lng }: { lng: string }) => {
+  const router = useRouter();
+
   return (
     <div className="flex flex-col">
       <label
@@ -16,13 +17,14 @@ const SelectLanguage = () => {
       <select
         name="Language"
         id="Language"
+        value={lng}
         className="mt-1.5 w-60 h-28 rounded-lg border-gray-300 text-gray-700 sm:text-8xl"
-        onChange={(e) => setLang(e.target.value)}
+        onChange={(e) => router.push(`/${e.target.value}`)}
       >
-        <option value="KR">KR</option>
-        <option value="EN">EN</option>
-        <option value="JP">JP</option>
-        <option value="CN">CN</option>
+        <option value="kr">KR</option>
+        <option value="en">EN</option>
+        <option value="jp">JP</option>
+        <option value="cn">CN</option>
       </select>
     </div>
   );
