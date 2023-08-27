402ede1a222ff3f94fbcb2de3b88000e603aeb8d
Hwang Sujung <crystalhwang0416@gmail.com>
2023-08-27 23:33:16 +0900

    fix: nav 경로 임시 변경

--- src/app/[lng]/nav.tsx
+++ src/app/[lng]/nav.tsx
@@ -1,25 +1,32 @@
 //FIXME:  href="/" 이동 경로 추가 필요, nav css 모바일 대응 필요
 
-const Nav = () => (
+const Nav = ({ lng }: { lng: string }) => (
   <nav aria-label="Global">
     <ul className="flex items-center gap-6 text-sm">
-      <li>
+      {/* <li>
         <a
           className="text-gray-500 transition hover:text-gray-500/75"
           href="https://github.com/shwang0416/KRplz"
         >
           About
         </a>
-      </li>
+      </li> */}
 
       <li>
-        <a className="text-gray-500 transition hover:text-gray-500/75">
+        <a
+          className="text-gray-500 transition hover:text-gray-500/75"
+          // href={`/${lng}/how-to-use`}
+          href="https://github.com/shwang0416/KRplz"
+        >
           How to use
         </a>
       </li>
 
       <li>
-        <a className="text-gray-500 transition hover:text-gray-500/75">
+        <a
+          className="text-gray-500 transition hover:text-gray-500/75"
+          href="https://github.com/shwang0416/KRplz"
+        >
           Get the App
         </a>
       </li>
